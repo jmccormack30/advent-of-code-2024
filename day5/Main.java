aa633d0ef5f62b9fc5d0ef1e03ee7b2051d77305
Jack McCormack <jack.mccormack1234@gmail.com>
2024-12-05 17:31:00 -0500

    Completed the solution for Part 2 of Day #5

--- day5/Main.java
+++ day5/Main.java
@@ -2,26 +2,25 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
         String fileName = "input.txt";
 
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            int value = decodeFilePart1(br);
-            System.out.println("Answer = " + value);
-        }
-        catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        int value = decodeFile(br);
+        System.out.println("Answer = " + value);
     }
 
-    private static int decodeFilePart1(BufferedReader br) throws Exception {
+    private static int decodeFile(BufferedReader br) throws Exception {
         String line;
         Map<Integer, Set<Integer>> pageOrderMap = new HashMap<>();
         List<List<Integer>> updatesList = new ArrayList<>();
@@ -39,21 +38,17 @@ class Main {
             }
             else {
                 String[] arr = line.split(",");
-                List<Integer> update = Arrays.stream(arr).map(str -> Integer.parseInt(str)).toList();
+                List<Integer> update = Arrays.stream(arr).map(str -> Integer.parseInt(str)).collect(Collectors.toList());
                 updatesList.add(update);
             }
         }
 
-        int total = 0;
+        SortByMap sortByMapComparator = new SortByMap(pageOrderMap);
 
-        for (List<Integer> update : updatesList) {
-            total += getValue(update, pageOrderMap);
-        }
-        
-        return total;
+        return updatesList.stream().mapToInt(list -> getValuePart2(list, pageOrderMap, sortByMapComparator)).sum();
     }
 
-    private static int getValue(List<Integer> update, Map<Integer, Set<Integer>> pageOrderMap) {
+    private static int getValuePart1(List<Integer> update, Map<Integer, Set<Integer>> pageOrderMap) {
         for (int i = update.size() - 1; i > 0; i--) {
             int cur = update.get(i);
             Set<Integer> nums = pageOrderMap.get(cur);
@@ -62,7 +57,44 @@ class Main {
                 if (invalid) return 0;
             }
         }
-        System.out.println(update);
         return update.get(update.size() / 2);
     }
-}
+
+    private static int getValuePart2(List<Integer> update, Map<Integer, Set<Integer>> pageOrderMap, SortByMap comparator) {
+        for (int i = update.size() - 1; i > 0; i--) {
+            int cur = update.get(i);
+            Set<Integer> nums = pageOrderMap.get(cur);
+            if (nums != null && !nums.isEmpty()) {
+                boolean invalid = update.subList(0, i).stream().anyMatch(n -> nums.contains(n));
+                if (invalid) {
+                    Collections.sort(update, comparator);
+                    return update.get(update.size() / 2);
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static class SortByMap implements Comparator<Integer> {
+
+        private Map<Integer, Set<Integer>> pageOrderMap;
+
+        public SortByMap(Map<Integer, Set<Integer>> pageOrderMap) {
+            this.pageOrderMap = pageOrderMap;
+        }
+
+        @Override
+        public int compare(Integer a, Integer b) {
+            Set<Integer> aNums = pageOrderMap.get(a);
+            Set<Integer> bNums = pageOrderMap.get(b);
+
+            boolean aLessThan = (aNums != null && aNums.contains(b));
+            boolean bLessThan = (bNums != null && bNums.contains(a));
+
+            if (!aLessThan && !bLessThan) return 0;
+            if (aLessThan && bLessThan) throw new RuntimeException("Invalid bi-directional relationship");
+
+            return aLessThan ? -1 : 1;
+        }
+    };
+}   
