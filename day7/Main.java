10d8dc59ac16308111b9a7a5b0a9cabad9faa799
Jack McCormack <jack.mccormack1234@gmail.com>
2024-12-13 11:31:49 -0500

    Added solution for part 2 of day7

--- day7/Main.java
+++ day7/Main.java
@@ -5,9 +5,8 @@ class Main {
 
     enum Operation {
         ADD,
-        SUB,
         MUL,
-        DIV
+        CONCAT
     }
 
 	public static void main(String[] args) throws Exception {
@@ -44,7 +43,7 @@ class Main {
         int total = nums[0];
         int i = 1;
 
-        long x = getValue(value, total, i, nums);
+        long x = getValuePart2(value, total, i, nums);
         return (x == value) ? value : 0;
     }
 
@@ -64,12 +63,35 @@ class Main {
         return 0;
     }
 
+    private static long getValuePart2(long value, long total, int i, int[] nums) {
+        if (i >= nums.length) return total;
+        int y = nums[i];
+        i += 1;
+
+        long sum = performOperation(total, y, Operation.ADD);
+        long sumTotal = getValuePart2(value, sum, i, nums);
+        if (sumTotal == value) return sumTotal;
+
+        long product = performOperation(total, y, Operation.MUL);
+        long productTotal = getValuePart2(value, product, i, nums);
+        if (productTotal == value) return productTotal;
+
+        long concat = performOperation(total, y, Operation.CONCAT);
+        long concatTotal = getValuePart2(value, concat, i, nums);
+        if (concatTotal == value) return concatTotal;
+
+        return 0;
+    }
+
     private static long performOperation(long x, long y, Operation operation) {
         switch (operation) {
             case ADD:
                 return (x + y);
             case MUL:
                 return (x * y);
+            case CONCAT:
+                String concat = String.valueOf(x) + String.valueOf(y);
+                return Long.parseLong(concat);
             default:
                 throw new IllegalArgumentException("Invalid operation: " + operation);
         }
